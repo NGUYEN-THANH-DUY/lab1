776ed5789ea7ecd27e6b04e21bf81f4bd0fb2877
Your NameduyÂgmail.codu <you@example.com>
2020-09-24 21:24:39 +0700

    them folder

---
+++ lab1/Test Suites/LAB1.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LAB1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>521ea852-7d67-442b-9271-82e0199132a8</testSuiteGuid>
+   <testCaseLink>
+      <guid>019f94af-3cb6-4d47-a5d3-a3a5574a4872</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4c043292-3068-4f56-b55b-859317156175</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dca6bc70-85c8-4234-9fe5-464400790178</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ef5fd220-b88d-4d69-95d9-1e7d8c469b51</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>00737395-8ed9-4337-8374-349ad7b220b1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
